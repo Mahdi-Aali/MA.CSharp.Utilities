78d5ad1bb29689d59cca104b1f118750c9f79e2d
mahdi <mahdi1383harold@outlook.com>
2024-01-31 01:28:04 +0330

    package initializing

---
+++ MA.CSharp.Utilities/Object/ObjectUtilities.cs
@@ -0,0 +1,64 @@
+﻿using MA.CSharp.Utilities.Text;
+using System.Reflection;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace MA.CSharp.Utilities.Object;
+
+public static class ObjectUtilities
+{
+    public static bool IsNull(this object obj) => obj == null;
+    public static bool IsNotNull(this object obj) => obj != null;
+    public static string SerializeAsJson(this object obj) => JsonSerializer.Serialize(obj);
+    public static T DeserializeFromJson<T>(this string jsonDocument) => JsonSerializer.Deserialize<T>(jsonDocument)!;
+    public static string SerializeAsXML(this object obj)
+    {
+        XmlSerializer xs = new(obj.GetType());
+        using (MemoryStream ms = new())
+        {
+            xs.Serialize(ms, obj);
+            return ms.ToArray().GetUtf8StringFromBytes();
+        }
+    }
+    public static T DeserializeFromXml<T>(this string xmlDocument)
+    {
+        XmlSerializer xs = new(typeof(T));
+        using (MemoryStream ms = new(xmlDocument.GetUtf8Bytes()))
+        {
+            return (T)xs.Deserialize(ms)!;
+        }
+    }
+    public static PropertyInfo[] GetProperties(this object obj) => obj.GetType().GetProperties();
+    public static PropertyInfo[] GetProperties(this object obj, BindingFlags bindingFlags) =>
+        obj.GetType().GetProperties(bindingFlags);
+
+    public static PropertyInfo GetProperty(this object obj, string propertyName) => obj.GetType().GetProperty(propertyName)!;
+    public static bool Is<T>(this object obj) => obj is T;
+    public static T GetPropertyValue<T>(this object obj, string propertyName)
+    {
+        try
+        {
+            return (T)obj.GetType().GetProperty(propertyName)?.GetValue(obj)!;
+        }
+        catch
+        {
+            return default(T)!;
+        }
+    }
+
+    public static void SetPropertyValue<T>(this object obj, string propertyName, T value)
+    {
+        try
+        {
+            obj.GetProperty(propertyName)?.SetValue(obj, value);
+        }
+        catch
+        {
+            // do nothing;
+        }
+    }
+    public static bool HasBaseType<T>(this object obj)
+    {
+        return obj.GetType().BaseType == typeof(T);
+    }
+}
---
+++ MA.CSharp.Utilities/Text/StringUtilities.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+namespace MA.CSharp.Utilities.Text;
+
+public static class StringUtilities
+{
+    public static bool IsNullOrEmpty(this string value) => string.IsNullOrEmpty(value);
+    public static bool IsNotNullOrEmpty(this string value) => !string.IsNullOrEmpty(value);
+    public static bool IsNullOrWhiteSpace(this string value) => string.IsNullOrWhiteSpace(value);
+    public static bool IsNotNullOrWhiteSpace(this string value) => !string.IsNullOrWhiteSpace(value);
+    public static byte[] GetUtf8Bytes(this string value) => Encoding.UTF8.GetBytes(value);
+    public static byte[] GetUtf32Bytes(this string value) => Encoding.UTF32.GetBytes(value);
+    public static string GetUtf8StringFromBytes(this byte[] bytes) => Encoding.UTF8.GetString(bytes);
+    public static string GetUtf32StringFromBytes(this byte[] bytes) => Encoding.UTF32.GetString(bytes);
+}
